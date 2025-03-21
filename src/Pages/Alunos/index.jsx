7a5f0c8c170ca639269522eb6d37c6610e399dce
vihugocosta <victorhugocosta02@gmail.com>
2025-03-20 22:21:50 -0300

    Update index.jsx
    initial alunos page

--- src/Pages/Alunos/index.jsx
+++ src/Pages/Alunos/index.jsx
@@ -1,7 +1,19 @@
 import "./Alunos.css";
 
 function Alunos() {
-  return <div>Alunos</div>;
+ return (<>
+    <h1>Alunos RPV</h1>
+    <div id="students-container" >
+      <div className="student-card" > Victor Hugo Costa Cruz Menezes</div>
+      <div className="student-card" >Pedro Henrique</div>
+      <div className="student-card" >Daniel Martins</div>
+      <div className="student-card" >Fulano</div>
+      <div className="student-card" >José Henriques</div>
+      <div className="student-card" >Carlinhos</div>
+      <div className="student-card" >Zé das couves</div>
+      <div className="student-card" >Roberto Justus</div>
+    </div>
+  </>)
 }
 
 export default Alunos;

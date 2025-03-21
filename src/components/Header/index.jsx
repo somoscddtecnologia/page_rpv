9a3bb65fe62e277ec52afbe957157725e43aab31
Aluno <aluno@senai.br>
2025-03-20 21:54:49 -0300

    HEADER

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+import {Link} from 'react-router-dom';
+ 
+function Header()  {
+return (
+
+  <div className="container-header">
+    <Link to="/">Técnico em Desenvolvimento de Sistemas</Link>
+    <Link to="/quemsomos">Quem somos</Link>
+    <Link to="/sobrenos">Sobre nós</Link>
+    <Link to="/alunos">Alunos</Link>
+  </div>
+) 
+
+}
+
+export default Header;
